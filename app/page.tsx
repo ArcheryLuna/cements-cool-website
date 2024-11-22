d63808e23bb82ef257575175f6bcdf3ee814d672
ArcheryLuna <me@archery-luna.com>
2024-11-22 09:22:29 +0000

    Cement

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,102 @@
 import Image from "next/image";
 
 import { ModeToggle } from "@/components/dark-mode-switcher";
+import { HeroParallax } from "@/components/heros/hero-paralax";
+
+const pictures: {
+    title: string;
+    link?: string | undefined;
+    image: string;
+}[] = [{
+    title: "Cowboy Time",
+    image: "/cowboy%20time.png"
+}, {
+    title: "Catfish sticker",
+    image: "/catfish%20sticker%202.png"
+}, {
+    title: "Cuddy",
+    image: "/cuddy.png"
+}, {
+    title: "hilson bleh bleh bleh",
+    image: "/hilson%20bleh%20bleh%20bleh.png"
+}, {
+    title: "house and wilson again",
+    image: "/house%20and%20wilson%20again.png"
+}, {
+    title: "house doodles",
+    image: "/house%20doodles.png"
+},{
+    title: "house in tub",
+    image: "/house%20in%20tub.png"
+}, {
+    title: "house white and freeman",
+    image: "/house%20white%20freeman.png"
+}, {
+    title: "Placeholder 9",
+    image: "/placeholder3.jpeg"
+},{
+    title: "Placeholder 10",
+    image: "/placeholder1.png"
+}, {
+    title: "Placeholder 11",
+    image: "/placeholder2.png"
+}, {
+    title: "Placeholder 12",
+    image: "/placeholder3.jpeg"
+},{
+    title: "Placeholder 13",
+    image: "/placeholder1.png"
+}, {
+    title: "Placeholder 14",
+    image: "/placeholder2.png"
+}, {
+    title: "Placeholder 15",
+    image: "/placeholder3.jpeg"
+}]
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord, faInstagram, faXTwitter, faTumblr, faSteam } from '@fortawesome/free-brands-svg-icons';
+
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div className="">
-        
-    </div>
-  );
+    return (
+        <div className="">
+        <HeroParallax products={pictures} /> 
+        <section className='my-6 lg:my-12 px-6 py-6 lg:px-12 lg:py-12 bg-neutral-200 dark:bg-neutral-900' id="social-media">
+        <div className="lg:grid-cols-4 lg:grid ">
+        <div className="col-start-2 col-span-2 justify-items-center grid grid-cols-1 gap-y-2 md:grid-cols-5">
+        <Link href="https://www.instagram.com/wprostvii/" className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faInstagram} className="text-2xl" />
+        <p>wprostvii</p>
+        </Link>
+        <Link href="https://x.com/wprost_VII" className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faXTwitter} className="text-2xl" />
+        <p>wprost VII</p>
+        </Link>
+        <Link href="https://www.tumblr.com/wprostvii" className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faTumblr} className="text-2xl" />
+        <p>wprostvii</p>
+        </Link>
+        <Link href="https://discord.gg/" className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faDiscord} className="text-2xl" />
+        <p>wprostvii</p>
+        </Link>
+        <Link href="https://steamcommunity.com/id/wprostvii" className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faSteam} className="text-2xl" />
+            <p>Cement Mix</p>
+        </Link>
+        </div>
+        </div>
+        </section>
+        <section id='quote'>
+            <div className="text-center font-serif text-5xl lg:text-7xl px-6 py-6 lg:px-12 lg:py-12">
+               <blockquote cite="https://wprostvii.neocities.org/">
+                &quot;The things you own, end up owning you.&quot;<br/> <span className='text-2xl text-gray-200 dark:text-gray-600'>1999 Film Fight Club</span>
+                </blockquote>
+            </div>
+        </section>
+
+        </div>
+    );
 }
--- components/headers/navbar.tsx
+++ components/headers/navbar.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import Link from "next/link"
 
 function NavigationBar() {
-    return <nav className="grid grid-cols-3 lg:grid-cols-12 text-center px-6 py-6 lg:px-12 lg:py-12">
-        <h1 className="col-span-3 lg:col-span-12 my-3 lg:mb-6 font-bold text-4xl">
-            WPROSTVII
-        </h1>
-        <Link href="/" className="lg:col-span-4">
-            <p>Home Page</p>
-        </Link>
-        <Link href="/art" className="lg:col-span-4">
-            <p>Art</p>
-        </Link>
-        <Link href="/about" className="lg:col-span-4">
-            <p>About Me</p>
-        </Link>
-    </nav>
+    return <header className="sticky top-0 w-full h-full z-20"> 
+        <div className="absolute h-full w-full bg-[radial-gradient(transparent_1px,_#0c0a09_1px)] bg-[length:4px_4px] backdrop-blur-sm mask-gradient-to-b"></div>
+        <nav className="relative z-30 grid grid-cols-3 lg:grid-cols-12 text-center w-full px-6 py-6 lg:px-12 lg:py-12">
+            <h1 className="col-span-3 lg:col-span-12 my-3 lg:mb-6 font-bold text-4xl">
+                WPROSTVII
+            </h1>
+            <Link href="/" className="lg:col-span-4">
+                <p className="hover:underline transition-all duration-200">Home Page</p>
+            </Link>
+            <Link href="/art" className="lg:col-span-4">
+                <p className="hover:underline transition-all duration-200">Art</p>
+            </Link>
+            <Link href="/about" className="lg:col-span-4">
+                <p className="hover:underline transition-all duration-200">About Me</p>
+            </Link>
+        </nav>
+    </header>
 }
 
 export default NavigationBar
